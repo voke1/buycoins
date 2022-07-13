37799d039f6f2ea2acffdaf56f1c8edb248e6752
voke <vokeolomu@yahoo.com>
2022-07-13 14:43:01 +0100

    clean up images

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,6 @@
 import constants from "./constants";
 import theme, { COLORS, SIZES, FONTS } from "./theme";
 import icons from "./icons";
-import images from "./images";
 import dummyData from "./dummyData";
 
-export { constants, theme, COLORS, SIZES, FONTS, icons, images, dummyData };
+export { constants, theme, COLORS, SIZES, FONTS, icons, dummyData };
